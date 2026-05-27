e07c5c1b0d9256a39b18ae5d57edbb648ac001f5
merhawitY <merhawit54tad@gmail.com>
2026-05-27 12:20:29 -0700

    badge

---
+++ badge.py
@@ -0,0 +1,6 @@
+employee = input("do you have a badge ? (yes/ no)")
+
+if employee == "yes" :
+    print("you can enter ")
+else:
+    print("go home")
---
+++ if.py
@@ -0,0 +1,7 @@
+input = input("whats is your score?")
+score = 85
+if score >= 50:
+     print("you almost fail but you passed")
+else :
+     print("you  failed")
+    
